8a186f48ff5c1def32422540eaf0a11f7ab4166b
Marcus Bernales <mboperator@gmail.com>
2017-06-01 11:14:19 -0700

    Setup client folder and hot loading

---
+++ web/client/App.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import View from './View';
+
+class App extends React.Component {
+  render() {
+    return (
+      <div>
+        Leggo.
+        <View />
+      </div>
+    )
+  }
+}
+
+export default App;
